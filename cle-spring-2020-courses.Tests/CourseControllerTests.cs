452fde437a2562aa28989db5447bff1ceaa96eb7
Jen E <jenevans3ten@gmail.com>
2020-02-26 12:00:08 -0500

    added NSubstitute and mocked CourseController tests

--- cle-spring-2020-courses.Tests/CourseControllerTests.cs
+++ cle-spring-2020-courses.Tests/CourseControllerTests.cs
@@ -4,16 +4,20 @@ using Microsoft.AspNetCore.Mvc;
 using cle_spring_2020_courses.Controllers;
 using cle_spring_2020_courses.Models;
 using System.Collections.Generic;
+using NSubstitute;
+using cle_spring_2020_courses.Repositories;
 
 namespace cle_spring_2020_courses.Tests
 {
     public class CourseControllerTests
     {
         CourseController controller;
+        IRepository<Course> courseMockRepo;
         
         public CourseControllerTests()
         {
-            // controller = new CourseController();
+            courseMockRepo = Substitute.For<IRepository<Course>>();
+            controller = new CourseController(courseMockRepo);
         }
 
         [Fact]
@@ -29,11 +33,16 @@ namespace cle_spring_2020_courses.Tests
         [Fact]
         public void Index_Passes_AllCourses_To_View()
         {
+            // Arrange
+            var expectedCourses = new List<Course>();
+            courseMockRepo.GetAll().Returns(expectedCourses);
+            
             // Act
             var result = controller.Index();
 
             // Assert
-            Assert.IsAssignableFrom<IEnumerable<Course>>(result.Model);
+            // Assert.IsAssignableFrom<IEnumerable<Course>>(result.Model);
+            Assert.Equal(expectedCourses, result.Model);
         }
 
         [Fact]
@@ -49,11 +58,16 @@ namespace cle_spring_2020_courses.Tests
         [Fact]
         public void Details_Passes_Course_To_View()
         {
+            // Arrange
+            var expectedCourse = new Course();
+            courseMockRepo.GetById(1).Returns(expectedCourse);
+
             // Act
             var result = controller.Details(1);
 
             // Assert
-            Assert.IsType<Course>(result.Model);
+            // Assert.IsType<Course>(result.Model);
+            Assert.Equal(expectedCourse, result.Model);
         }
 
     }
